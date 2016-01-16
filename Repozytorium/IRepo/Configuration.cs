1dda4d4b50672e7a73b54fb84ff6d645104817e0
SzymR <szymonrusz@gmail.com>
2016-01-16 20:24:16 +0100

    1 commit

---
+++ Repozytorium/IRepo/Configuration.cs
@@ -0,0 +1,153 @@
+namespace Repozytorium.Migrations
+{
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Repozytorium.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Repozytorium.Models.OglContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+            
+            
+        }
+
+        protected override void Seed(Repozytorium.Models.OglContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+
+            // Do debugowania metody seed
+            // Zastosowanie tego kodu podczas startu metody Seed() uruchomi druga instancje VisualStudio,
+            // w ktorej bedzie mozliwosc debugowania i sprawdzenia, jakie bledy zawiera metoda.
+
+            //if (System.Diagnostics.Debugger.IsAttached == false)
+            //    System.Diagnostics.Debugger.Launch();
+
+            SeeedRoles(context);
+            SeedUsers(context);
+            SeedOgloszenia(context);
+            SeedKategorie(context);
+            SeedOgloszenie_Kategoria(context);
+        }
+
+        private void SeedUsers(Models.OglContext context)
+        {
+            var store = new UserStore<Uzytkownik>(context);
+            var manager = new UserManager<Uzytkownik>(store);
+            if (!context.Users.Any(u => u.UserName == "Administrator"))
+            {
+                var user = new Uzytkownik { UserName = "Admin@asp.pl", Wiek=21 };
+                var adminresult = manager.Create(user, "12345678");
+                if (adminresult.Succeeded)
+                {
+                    manager.AddToRole(user.Id, "Admin");
+                }
+            }
+            if(!context.Users.Any(u=>u.UserName=="Marek")){
+                var user = new Uzytkownik {UserName="marek@asp.pl"};
+                var adminresult = manager.Create(user,"1234Abc");
+                if(adminresult.Succeeded){
+                    manager.AddToRole(user.Id, "Pracownik");
+                }
+
+            }
+            if(!context.Users.Any(u=>u.UserName=="Prezes")){
+                var user = new Uzytkownik {UserName="prezes@asp.pl"};
+                var adminresult = manager.Create(user,"1234Abc");
+                if(adminresult.Succeeded){
+                    manager.AddToRole(user.Id, "Admin");
+                }
+            }
+
+
+        }
+         //AddOrUpdate nie bedzie duplikowac danych przy kazdym wywolaniu metody Seed()
+        private void SeedOgloszenia(Models.OglContext context)
+        {
+            var idUzytkownika = context.Set<Uzytkownik>().Where(u => u.UserName == "Admin").FirstOrDefault().Id;
+            for (int i = 1; i < 10; i++)
+            {
+                var ogl = new Ogloszenie(){
+                    Id = i,
+                    UzytkownikId = idUzytkownika,
+                    Tresc = "Treœæ og³oszenia" + i.ToString(),
+                    Tytul = "Tytu³ og³oszenia" + i.ToString(),
+                    DataDodania = DateTime.Now.AddDays(-i)
+                };
+                context.Set<Ogloszenie>().AddOrUpdate(ogl);
+            }
+            context.SaveChanges();            
+        }
+
+        private void SeedKategorie(Models.OglContext context)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                var kat = new Kategoria()
+                {
+                    Id = i,
+                    Nazwa = "Nazwa kategorii" + i.ToString(),
+                    Tresc = "Treœæ og³oszenia" + i.ToString(),
+                    MetaTytul = "Tytu³ kategorii" + i.ToString(),
+                    MetaOpis = "Opis kategorii" + i.ToString(),
+                    MetaSlowa = "S³owa kluczowe do kategorii" + i.ToString(),
+                    ParentId = i
+                };
+                context.Set<Kategoria>().AddOrUpdate(kat);
+            }
+            context.SaveChanges();
+        }
+
+        private void SeedOgloszenie_Kategoria(Models.OglContext context)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                var okat = new Ogloszenie_Kategoria()
+                {
+                    Id = i,
+                    OgloszenieId = i / 2 + 2,
+                    KategoriaId = i / 2 + 2,
+                };
+                context.Set<Ogloszenie_Kategoria>().AddOrUpdate(okat);
+            }
+            context.SaveChanges();
+        }
+
+        private void SeeedRoles(Models.OglContext context)
+        {
+            var roleManager = new RoleManager<Microsoft.AspNet.Identity.EntityFramework.IdentityRole>
+            (new RoleStore<IdentityRole>());
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists("Pracownik"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Pracownik";
+                roleManager.Create(role);
+            }
+            
+        }
+    }
+}
---
+++ Repozytorium/Repo/OgloszenieRepo.cs
@@ -0,0 +1,82 @@
+﻿using Repozytorium.IRepo;
+using Repozytorium.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Repozytorium.Repo
+{
+    public class OgloszenieRepo : IOgloszenieRepo
+    {
+        private readonly IOglContext _db;
+
+        /// <summary>
+        /// Konstruktor w ktorym wstrzykiwana jest instancja repozytorium
+        /// </summary>
+        /// <param name="db">Baza</param>
+        public OgloszenieRepo(IOglContext db)
+        {
+            _db = db;
+        }
+        
+        public IQueryable<Ogloszenie> PobierzOgloszenia()
+        {           
+            //_db.Database.Log = message => Trace.WriteLine(message); // sledzenie zapytan wysylanych do bazy
+            //var ogloszenia = db.Ogloszenia.Include(o => o.Uzytkownik);
+           
+            var ogloszenia = _db.Ogloszenia.AsNoTracking();
+            return ogloszenia;
+        }
+        public Ogloszenie GetOgloszenieById(int id)
+        {
+            Ogloszenie ogloszenie = _db.Ogloszenia.Find(id);
+            return ogloszenie;
+        }
+
+        
+        public void UsunOgloszenie(int id)
+        {
+            UsunPowiazanieOgloszenieKategoria(id);
+            Ogloszenie ogloszenie = _db.Ogloszenia.Find(id);
+            _db.Ogloszenia.Remove(ogloszenie);            
+        }
+
+        
+        private void UsunPowiazanieOgloszenieKategoria(int idOgloszenia)
+        {
+            var list = _db.Ogloszenie_Kategoria.Where(o => o.OgloszenieId == idOgloszenia);
+            foreach (var el in list)
+            {
+                _db.Ogloszenie_Kategoria.Remove(el);
+            }
+        }
+
+        
+        public IQueryable<Ogloszenie> PobierzStrone(int? page = 1, int? pageSize = 10)
+        {
+            var ogloszenia = _db.Ogloszenia
+                .OrderByDescending(o => o.DataDodania) // Sortowanie malejąco, od najnowszych ogłoszeń
+                .Skip((page.Value - 1) * pageSize.Value) // Opuszcza x wybranych elementów
+                .Take(pageSize.Value); // Pobranie tylko x najnowszych
+            return ogloszenia;
+        }
+
+        
+        public void SaveChanges()
+        {
+            _db.SaveChanges();
+        }
+        
+        public void Dodaj(Ogloszenie ogloszenie)
+        {
+            _db.Ogloszenia.Add(ogloszenie);
+        }
+
+        public void Aktualizuj(Ogloszenie ogloszenie)
+        {
+            _db.Entry(ogloszenie).State = EntityState.Modified;
+        }
+    }
+}
